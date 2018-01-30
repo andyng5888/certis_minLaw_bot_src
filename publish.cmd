3590e71156bbfd5f158968028e7a9dec27de940a
andyng5888 <35707417+andyng5888@users.noreply.github.com>
2018-01-30 09:13:25 +0800

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=certis-test-bot-Web-Deploy.pubxml -p:Password=fwnz3u11r6qztp5ncR4yMvin6dXmHdjFj305aFxd96Zyvj1r3J8kFE1mpquZ
+
